571e7d94e40c4a4e6befcf582205d8fd62d681e8
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-03-13 15:18:02 +0100

    generacion pagina miembros

--- grics_laravel/grics_react/src/Components/Membres.jsx
+++ grics_laravel/grics_react/src/Components/Membres.jsx
@@ -1,10 +1,32 @@
-
+import Menu from "../Navbar/Menu";
+import Footer from "../Footer/Footer";
 import Comp_Membres from './MiembroComp/MiembroComp'
 function Membre() {
   return (
-    <>
-      <Comp_Membres/>
-    </>
+    <div className="paginaMembres">
+    <Menu />
+
+
+      <div className="contenedorMembres">
+          <div className="img">
+              <img className="generalMiembros" src={fotoPrincipal} alt="foto principal" />
+          </div>
+          <div>
+            <Comp_Membres/>
+          </div>
+          <div>
+            <Comp_Membres/>
+          </div>
+          <div>
+            <Comp_Membres/> 
+          </div>
+          <div>
+            <Comp_Membres/>
+          </div>
+      </div>
+    <Footer />
+    </div>
+
   )
 }
 
