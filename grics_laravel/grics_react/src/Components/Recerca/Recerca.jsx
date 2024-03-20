d9d41c0c936968db2ae4921840666086b7a42215
= <=>
2024-03-20 16:29:29 +0100

    Inicio de la pagina Recerca

--- grics_laravel/grics_react/src/Components/Recerca/Recerca.jsx
+++ grics_laravel/grics_react/src/Components/Recerca/Recerca.jsx
@@ -2,10 +2,21 @@ import './Recerca.css'
 import Menu from '../Navbar/Menu'
 import Footer from '../Footer/Footer'
 
+
 export default function Recerca() {
     return (
         <div className="recerca">
             <Menu/>
+            <div className='titulo'>
+                <div className='line-1'></div>
+                <h1 className='pppp'>RECERCA</h1>
+                <div className='line-2'></div>
+
+            </div>
+            <div className='linias'>
+                <h2 className='titulo-Linias'>LÍNIES D'INVESTIGACIÓ</h2>
+            </div>
+            <div className='carousel-Linias'></div>
             <Footer/>
         </div>
     )
