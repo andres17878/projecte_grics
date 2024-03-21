2002dea6cf27c906545d9e12389fb9bca7becc76
Andres <cf22andres.zambrano@iesjoandaustria.org>
2024-03-21 16:25:42 +0100

    Página de publicacions a la mitad, se hace correctamente el fetch a la base de datos a través de laravel y se renderizan uno a uno, falta la paginación y pulir el apartado estético

--- grics_laravel/routes/api.php
+++ grics_laravel/routes/api.php
@@ -3,17 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -28,4 +17,28 @@ Route::get('prueba', function(Request $request){
 Route::get('allMembres', function(Request $request){
     $membres = \DB::table('membres')->get();
     return response() ->json($membres);
+});
+
+Route::get('allPublicacions', function(Request $request){
+    $publicacions = \DB::table('publicacions')->get();
+    return response() ->json($publicacions);
+});
+
+
+Route::get('publicacio/{id}', function(Request $request, $id){
+    $publicacio = \DB::table('publicacions')->where('id', $id)->first();
+    return response() ->json($publicacio);
+});
+
+Route::get('countPublicacions', function(Request $request){
+    $count = \DB::table('publicacions')->count();
+    return response() ->json($count);
+});
+
+Route::get('allPublicacionsByMember/{id}', function(Request $request, $id){
+    $publicacions = \DB::table('membres_publicacions')
+    ->join('publicacions', 'membres_publicacions.publicacio_id', '=', 'publicacions.id')
+    ->where('membres_publicacions.membre_id', $id)
+    ->get();
+    return response() ->json($publicacions);
 });
