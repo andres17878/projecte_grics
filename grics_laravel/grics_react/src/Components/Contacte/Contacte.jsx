dea29dd4c162cacc09b3788c7c9ec698c156235d
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-03-21 16:35:22 +0100

    modifacada pagina contacte, con mapa y info añadida

--- grics_laravel/grics_react/src/Components/Contacte/Contacte.jsx
+++ grics_laravel/grics_react/src/Components/Contacte/Contacte.jsx
@@ -2,17 +2,42 @@ import './Contacte.css'
 import Menu from '../Navbar/Menu'
 import Footer from '../Footer/Footer'
 import Contacte_C from '../ContacteComp/ContacteComp'
+import map from '../../assets/map/mapUB.png'
 export default function Contacte() {
     return (
         <div className="contacte">
             <Menu/>
             <br/>
-            <div className='<Contacte_C/>'>
-                <Contacte_C/>
-            </div>
             
-            <br/>
+
+            <div className='ContacteC'>
+                <Contacte_C/>
+                <div className='info'>
+                    <div className="imgMap">
+                        <img className="mapUB" src={map} alt="foto_mapa" />
+                    </div>
+                    <div className='plusInfo'>
+                        <p>Direccion: ......</p><br/>
+                        <p>@ Correo : ..........</p><br/>
+                        <p>Telefono : ..........</p><br/>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className='info'>
+                <div className="imgMap">
+                    <img className="mapUB" src={map} alt="foto_mapa" />
+                </div>
+                <div className='plusInfo'>
+                    <p>Direccion: ......</p><br/>
+                    <p>@ Correo : ..........</p><br/>
+                    <p>Telefono : ..........</p><br/>
+                </div>
+            </div> */}
+            
+            
             <Footer/>
         </div>
+
     )
 }
