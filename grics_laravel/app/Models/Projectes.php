920186e831fb741429a079fb974efec0876d4123
Andres <cf22andres.zambrano@iesjoandaustria.org>
2024-03-22 16:51:01 +0100

    Base de datos terminada, migrations terminadas, seeders para testing terminados

---
+++ grics_laravel/app/Models/Projectes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Projectes extends Model
+{
+    use HasFactory;
+
+    protected $table = "projectes";
+
+    protected $fillable = ['foto', 'titol', 'resum', 'entitats', 'resultats', 'logos_entitats', 'data'];
+
+
+    public function membres(): BelongsToMany
+    {
+        return $this->belongsToMany(Membres::class, 'membres_projectes');
+    }
+}
---
+++ grics_laravel/database/seeders/ProjectesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            \DB::table("projectes")->insert([
+                'foto' => $faker->imageUrl,
+                'titol' => $faker->sentence,
+                'resum' => $faker->text,
+                'entitats' => $faker->company,
+                'resultats' => $faker->text,
+                'logos_entitats' => $faker->imageUrl,
+                'data' => $faker->date,
+            ]);
+        }
+
+    }
+}
