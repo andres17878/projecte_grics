a621b51c31e7fd89776fa67e89c692ec721dae77
= <=>
2024-04-08 16:39:36 +0200

    Carpeta con los botones de todos los carousels

---
+++ grics_laravel/grics_react/src/Components/Recerca/Botones-Carousel/CustomButtonGroup-Contractes.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import '../Carousel/Carousel.css';
+
+const CustomNavigationButtons = ({ next, previous, ...rest}) => {
+  const {
+    carouselState: { currentSlide },
+  } = rest;
+  
+  return(
+    <div className="grupo_botones_contractes">
+
+      <div className="boton_atras">
+        <button className="estiloBotones" onClick={() => previous()}>
+          {" "}
+          <FiChevronLeft className='flechasCarousel' />
+        </button>
+      </div>
+
+      <div className="boton_adelante">
+
+        <button className="estiloBotones" onClick={() => next()}>
+          {" "}
+          <FiChevronRight className='flechasCarousel'/>
+        </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default CustomNavigationButtons;
+
+CustomNavigationButtons.propTypes = {
+  next: PropTypes.func,
+  previous: PropTypes.func,
+  goToSlide: PropTypes.func
+}
