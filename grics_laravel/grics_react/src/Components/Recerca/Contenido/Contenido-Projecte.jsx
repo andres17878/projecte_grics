2bd32afd14fd69bbb99d622ea4857b96ddd0dd39
= <=>
2024-04-11 15:07:14 +0200

    En proceso de crear la página especifica de los proyectos

--- grics_laravel/grics_react/src/Components/Recerca/Contenido/Contenido-Projecte.jsx
+++ grics_laravel/grics_react/src/Components/Recerca/Contenido/Contenido-Projecte.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../Recerca.css'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Link as Proyecto} from 'react-router-dom';
 
 
 export default function Contenido(props) {
@@ -10,7 +11,7 @@ export default function Contenido(props) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/linies/${props.id}`)
+        axios.get(`http://localhost:8000/api/projectes/${props.id}`)
         .then((response) => {
             setData(response.data);
             setLoading(false);
@@ -26,14 +27,16 @@ export default function Contenido(props) {
 
   return (
     <div>
-        <div className='Card-2'>
-            <div className='Izquierda-Card-2'>
-                <img src={data.foto} alt='prueba' className='foto-Card'></img>
+        <Proyecto to={{pathname: "/projecte", state: {id: props.id}}}>
+            <div className='Card-2'>
+                <div className='Izquierda-Card-2'>
+                    <img src={data.foto} alt='prueba' className='foto-Card'></img>
+                </div>
+                <div className='Derecha-Card-2'>
+                    <h4 className='descripcion-Card'>{data.resum}</h4>
+                </div>
             </div>
-            <div className='Derecha-Card-2'>
-                <h4 className='descripcion-Card'>{data.descripcio}</h4>
-            </div>
-        </div>
+        </Proyecto>
     </div>
   )
 }
