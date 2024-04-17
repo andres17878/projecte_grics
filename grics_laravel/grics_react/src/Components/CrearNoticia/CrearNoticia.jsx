7c9715feebc46a59f92566547db092a3b3e671c4
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-04-17 16:28:48 +0200

    pàgina de creació de noticies en procés

---
+++ grics_laravel/grics_react/src/Components/CrearNoticia/CrearNoticia.jsx
@@ -0,0 +1,34 @@
+import './CrearNoticia.css'
+import Boton from '../Boton/Botton'
+
+function CrearNoticia() {
+    return (
+        <div className='divForm'>
+            <div className="spaceContacte">
+                <form className='form'>
+                    <div>
+                        <label htmlFor="nombre">Nom:</label>
+                        <input type="text" id="nombre" name="nombre" placeholder="Nom" />
+                    </div>
+                    <div>
+                        <label htmlFor="apellido">Cognom:</label>
+                        <input type="text" id="apellido" name="apellido" placeholder="Cognom" />
+                    </div>
+                    <div>
+                        <label htmlFor="consulta">Escriu aqui la teva consulta::</label>
+                        <textarea id="consulta" name="consulta" placeholder='...' />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="terminos" name="aceptaTerminos"/>
+                        <label htmlFor="terminos">Aceptar términos i condicions</label>
+                    </div>
+                    <Boton/>
+                </form>
+            </div>
+        </div>
+    )
+
+
+
+
+}
