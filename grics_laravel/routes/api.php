fd76bfe2ed92adb7c34eca64093358c7dc416bd7
Andres <cf22andres.zambrano@iesjoandaustria.org>
2024-04-22 18:12:49 +0200

    Se añade un login rudimentario, se añaden dependencias para el login, se cambia en el backend las rutas para gestionar el login y se implementa correctamente con JWT el login

--- grics_laravel/routes/api.php
+++ grics_laravel/routes/api.php
@@ -93,6 +93,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(AuthController::class)->group(function(){
     Route::post('register','register');
     Route::post('login','login');
-    Route::get('usetdetail','userDetails');
+    Route::get('userdetail','userDetails');
 });
 
