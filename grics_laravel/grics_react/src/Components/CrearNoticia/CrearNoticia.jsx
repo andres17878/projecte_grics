67753f0292db65ece4d444f865c65a83b23d14ce
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-05-08 16:30:05 +0200

    formulari afegirNoticia avançat

--- grics_laravel/grics_react/src/Components/CrearNoticia/CrearNoticia.jsx
+++ grics_laravel/grics_react/src/Components/CrearNoticia/CrearNoticia.jsx
@@ -1,34 +1,60 @@
-import './CrearNoticia.css'
-import Boton from '../Boton/Botton'
+import './FormMembres.css'
+import { useState } from 'react';
 
-function CrearNoticia() {
+function formulari_noticia() {
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleImageChange = (e) => {
+        e.preventDefault();
+        const file = e.dataTransfer.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImagePreview(reader.result);
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            setImagePreview(null);
+        }
+    }
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div className='divForm'>
-            <div className="spaceContacte">
-                <form className='form'>
-                    <div>  
-                        <label htmlFor="nombre">Nom:</label>
-                        <input type="text" id="nombre" name="nombre" placeholder="Nom" />
+        <div className='Formulario'>
+            <div className="spaceForm">
+                <form className='formM'>
+                    <div className='titolFormM'>
+                        <h1>NOVA NOTICIA</h1>
                     </div>
                     <div>
-                        <label htmlFor="apellido">Cognom:</label>
-                        <input type="text" id="apellido" name="apellido" placeholder="Cognom" />
+                        <input type="text" id="titulo" name="titulo" placeholder="Títol de la noticia" />
                     </div>
                     <div>
-                        <label htmlFor="consulta">Escriu aqui la teva consulta::</label>
-                        <textarea id="consulta" name="consulta" placeholder='...' />
+                        <input type="text" id="data" name="data" placeholder="Data de publicació" />
                     </div>
                     <div>
-                        <input type="checkbox" id="terminos" name="aceptaTerminos"/>
-                        <label htmlFor="terminos">Aceptar términos i condicions</label>
+                        <input type="text" id="descripcio" name="descripcio" placeholder="Text de la noticia" />
                     </div>
-                    <Boton/>
+                    <div 
+                        onDrop={handleImageChange} 
+                        onDragOver={handleDragOver} 
+                        style={{height: '200px', width: '200px', border: '1px dashed #ccc'}}
+                    >
+                        {imagePreview && (
+                            <img src={imagePreview} alt="Imagen seleccionada" />
+                        )}
+                    </div>
+                    <div className="button">
+                        <button type="submit" className="addMembre">Publica</button>
+                    </div>
                 </form>
             </div>
         </div>
     )
-
-
-
-
 }
+export default Form_M
