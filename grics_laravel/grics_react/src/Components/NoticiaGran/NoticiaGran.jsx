db9d4b88c3476ab91b6edf3899dda2255816f4ef
Andres <cf22andres.zambrano@iesjoandaustria.org>
2024-05-21 16:47:39 +0200

    Cambios en la noticia completa para que se muestre mejor

--- grics_laravel/grics_react/src/Components/NoticiaGran/NoticiaGran.jsx
+++ grics_laravel/grics_react/src/Components/NoticiaGran/NoticiaGran.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 import './NoticiaGran.css';
 import { Link, useParams } from "react-router-dom";
 import Footer from '../Footer/Footer'
@@ -36,7 +35,7 @@ export default function NoticiaGran() {
           <Link to="/actualitat"> <img src='../src/assets/BACK/arrow_back.svg' alt='Atras' className='imagen-atras'></img></Link>
         </div>
         <h1 className='titol'>{data.titol}</h1>
-        <p className='data'>Published {data.data}</p>
+        <p className='data'>Published {data.created_at}</p>
         <img className='imatge' src={data.foto} alt={data.titol} />
         <p className='descripcio'>{data.descripcio}</p>
       </div>
